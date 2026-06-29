b1bcd09331835f26c87ba3bc3f4f7aaca7d8282b
Hardik Pandey <albatrosshardik@gmail.com>
2026-06-29 18:29:05 +0100

    updated IP stats design

--- js/visitor-info.js
+++ js/visitor-info.js
@@ -47,17 +47,38 @@
     var coords = '';
     if (lat && lon) coords = lat + ',' + lon;
 
-    var html = '<div class="vi-row"><span class="vi-icon">\u25C8</span><span class="vi-label">IP</span><span class="vi-val">' + ip + '</span></div>';
-    if (loc) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Location</span><span class="vi-val vi-loc">' + loc + '</span></div>';
-    if (region) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Region</span><span class="vi-val vi-org">' + region + '</span></div>';
-    if (tz) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Timezone</span><span class="vi-val vi-org">' + tz + '</span></div>';
-    if (postal) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Postal</span><span class="vi-val vi-org">' + postal + '</span></div>';
-    if (hostname) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Hostname</span><span class="vi-val vi-org">' + hostname + '</span></div>';
-    if (coords) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Coords</span><span class="vi-val vi-org">' + coords + '</span></div>';
-    if (asn) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">ASN</span><span class="vi-val vi-org">AS' + asn + '</span></div>';
-    if (orgName) html += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">ISP</span><span class="vi-val vi-org">' + orgName + '</span></div>';
-    el.innerHTML = html;
+    var headerRow = '<div class="vi-row">';
+    headerRow += '<span class="vi-icon">\u25C8</span><span class="vi-label">IP</span><span class="vi-val">' + ip + '</span>';
+    headerRow += '</div>';
+
+    var bodyRows = '';
+    if (loc) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Location</span><span class="vi-val vi-loc">' + loc + '</span></div>';
+    if (region) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Region</span><span class="vi-val vi-org">' + region + '</span></div>';
+    if (tz) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Timezone</span><span class="vi-val vi-org">' + tz + '</span></div>';
+    if (postal) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Postal</span><span class="vi-val vi-org">' + postal + '</span></div>';
+    if (hostname) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Hostname</span><span class="vi-val vi-org">' + hostname + '</span></div>';
+    if (coords) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">Coords</span><span class="vi-val vi-org">' + coords + '</span></div>';
+    if (asn) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">ASN</span><span class="vi-val vi-org">AS' + asn + '</span></div>';
+    if (orgName) bodyRows += '<div class="vi-row"><span class="vi-icon-placeholder"></span><span class="vi-label">ISP</span><span class="vi-val vi-org">' + orgName + '</span></div>';
+
+    el.innerHTML = '<div class="vi-header">' + headerRow + '<button class="vi-toggle" aria-label="Toggle details">\u2212</button></div><div class="vi-body">' + bodyRows + '</div>';
     el.classList.add('vi-ready');
+
+    var toggle = el.querySelector('.vi-toggle');
+    var body = el.querySelector('.vi-body');
+
+    if (localStorage.getItem('vi-collapsed') !== 'false') {
+      el.classList.add('vi-collapsed');
+      toggle.textContent = '+';
+      if (body) body.style.display = 'none';
+    }
+
+    toggle.addEventListener('click', function () {
+      var collapsed = el.classList.toggle('vi-collapsed');
+      toggle.textContent = collapsed ? '+' : '\u2212';
+      if (body) body.style.display = collapsed ? 'none' : '';
+      localStorage.setItem('vi-collapsed', collapsed);
+    });
   }
 
   fetch('https://ipapi.co/json/')
